b3c2b95d5ebe7cc89f5c7283b7bda0a4af9d8726
Yeon <giojio825@gmail.com>
2023-10-04 08:25:47 +0700

    updet

--- daspro_jobsheet6/Pemilihan2Percobaan113.java
+++ daspro_jobsheet6/Pemilihan2Percobaan113.java
@@ -11,9 +11,14 @@ public class Pemilihan2Percobaan113 {
         tahun = input13.nextInt();
 
         if(tahun % 4 == 0) {
-            if(tahun%100 != 0)
-            System.out.print("Tahun Kabisat");
-        } else
-        System.out.println("Bukan Tahun Kabisat");
+            if(tahun%100 == 0){
+                if(tahun%400 == 0){
+                    System.out.println("Tahun Kabisat");
+                } else{
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            }else
+            System.out.println("Bukan Tahun Kabisat");
+        } 
     }
 }
