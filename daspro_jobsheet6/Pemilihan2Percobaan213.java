192d6f907597994a8c3931d622d78bbccfc6ddad
Yeon <giojio825@gmail.com>
2023-10-04 09:01:15 +0700

    update segi three

--- daspro_jobsheet6/Pemilihan2Percobaan213.java
+++ daspro_jobsheet6/Pemilihan2Percobaan213.java
@@ -16,12 +16,25 @@ public class Pemilihan2Percobaan213 {
 
         totSudut = sudut1 + sudut2 + sudut3;
 
-        if(totSudut == 180){
-            if ((sudut1 == 90) || (sudut1 == 90) || (sudut1 == 90))
-            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            else
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-        } else
-        System.out.println("Bukan Segitiga");
+        if (totSudut == 180) {
+            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
+                System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            } else if ((sudut1 == sudut2) || (sudut2 == sudut3) || (sudut3 == sudut1)) {
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            } else 
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
+            
+        } else {
+            System.out.println("Bukan Segitiga");
+        }
+
     }
 }
+
+        // if(totSudut == 180){
+        //     if ((sudut1 == 90) || (sudut1 == 90) || (sudut1 == 90))
+        //     System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+        //     else
+        //         System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+        // } else
+        // System.out.println("Bukan Segitiga");
