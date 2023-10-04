321e9f3b7056df725a463c59f71be304cbafa6df
Yeon <giojio825@gmail.com>
2023-10-04 09:37:19 +0700

    aku kaya karena pebisnis

---
+++ daspro_jobsheet6/Pemilihan2Percobaan313.java
@@ -0,0 +1,37 @@
+package jobsheet.daspro_jobsheet6;
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan313 {
+    public static void main(String[] args) {
+        Scanner input13 = new Scanner(System.in);
+
+        String kategori;
+        int penghasilan, gajiBersih;
+        double pajak = 0;
+
+        System.out.println("Masukkan Kategori :");
+        kategori = input13.nextLine();
+        System.out.println("Masukkan Besarnya Penghasilan :");
+        penghasilan = input13.nextInt();
+
+        if(kategori.equals("Pekerja")){
+            if (penghasilan <= 2000000)
+                pajak = 0.1;
+            else if(penghasilan <= 3000000)
+                pajak = 0.15;
+            else
+                pajak = 0.2;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Penghasilan Bersih :" + gajiBersih);
+        } else if (kategori.equals("pebisnis")){
+             if (penghasilan <= 2500000)
+                pajak = 0.14;
+            else if(penghasilan <= 3500000)
+                pajak = 0.2;
+            else
+                pajak = 0.25;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Penghasilan Bersih :" + gajiBersih);
+        }      
+    }
+}
