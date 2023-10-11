a4a014c8957eacd2f8bbddfc45f07b5e10bde9b1
Yeon <giojio825@gmail.com>
2023-10-11 07:29:20 +0700

    rata"

--- daspro_jobsheet7/ForKelipatan13.java
+++ daspro_jobsheet7/ForKelipatan13.java
@@ -4,7 +4,7 @@ public class ForKelipatan13 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int kelipatan, jumlah = 0, counter = 0;
+        int kelipatan, rata2, jumlah = 0, counter = 0;
 
         System.out.println("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
@@ -15,9 +15,11 @@ public class ForKelipatan13 {
                 counter++;
             }
         }
+        rata2 = jumlah / counter;
 
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adlaah %d\n", kelipatan, jumlah);
+        System.out.println("Rata-rata dari seluruh bilangan kelipatan: " + rata2);
 
     }
 }
