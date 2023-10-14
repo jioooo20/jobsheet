3d7b6c5aebee00f81ac1a8c31cebdbed8138990a
Yeon <giojio825@gmail.com>
2023-10-14 11:02:09 +0700

    cuti dulu baaaannhhh

--- daspro_jobsheet7/DoWhileCuti13.java
+++ daspro_jobsheet7/DoWhileCuti13.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
+
 public class DoWhileCuti13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int jatahCuti, jumHari;
         String konfirmasi;
-
         System.out.print("Jatah cuti: ");
         jatahCuti = sc.nextInt();
 
         do{
             System.out.print("Apakah Anda ingin mengambil cuti(y/t) ? ");
             konfirmasi = sc.next();
-
             if (konfirmasi.equalsIgnoreCase("y")){
                 System.out.print("Jumlah Hari: ");
                 jumHari = sc.nextInt();
-
+                // do{
                 if(jumHari<=jatahCuti){
                     jatahCuti -= jumHari;
                     System.out.println("Sisa jatah cuti: " + jatahCuti);
                 } else{
                     System.out.println("Sisa jatah cuti anda tidak mencukupi");
-                    break;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                    continue;
                 }
+            // }while(jatahCuti!=0);
             }
-        } while (jatahCuti>0);
+        } while (jatahCuti!=0);
     }
 }
