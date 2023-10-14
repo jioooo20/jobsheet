37ed5c52ff7fed05f961f6d0ae3ac7aa8e5132ba
Yeon <giojio825@gmail.com>
2023-10-14 10:40:56 +0700

    yeah

--- daspro_jobsheet7/WhileGaji13.java
+++ daspro_jobsheet7/WhileGaji13.java
@@ -1,35 +1,35 @@
 import java.util.Scanner;
-
 public class WhileGaji13 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         String jabatan;
         int jumKaryawan, jumJamLembur;
-        double gajiLembur = 0, totGajiLembur = 0;
+        double gajiManager = 0, totGajiLembur = 0, gajiKaryawan = 0;
         
         System.out.print("Masukkan jumlah karyawan: ");
         jumKaryawan = scan.nextInt();
 
         int i=0;
         while (i < jumKaryawan){
-            System.out.print("Pilihan jabatan - Direktur, Manager, Karyawan");
+            System.out.println("Pilihan jabatan - Direktur, Manager, Karyawan");
             System.out.print("Masukkan jabatan karyawan ke-"+ (i+1) + ": ");
             jabatan = scan.next();
             System.out.print("Masukkan jumlah jam lembur: ");
-            jumJamLembur = scan.nextInt();
-            i++;
+            jumJamLembur = scan.nextInt();       
 
             if(jabatan.equalsIgnoreCase("direktur")){
-                continue;
+                System.out.print("");                        
             } else if (jabatan.equalsIgnoreCase("manager")){
-                gajiLembur = jumJamLembur * 100000;
+                gajiManager = jumJamLembur * 100000;
             } else if (jabatan.equalsIgnoreCase("karyawan")){
-                gajiLembur = jumJamLembur * 75000;
+                gajiKaryawan = jumJamLembur * 75000;
+            } else {
+                System.out.println("Jabtan Invalid");
+                continue;
             }
-            totGajiLembur += gajiLembur;
-            System.out.print("Total gaji lembur: " + totGajiLembur);
+            i++;            
         }
-
+            totGajiLembur += gajiKaryawan + gajiManager;
+            System.out.println("Total gaji lembur: " + totGajiLembur);                       
     }
 }
