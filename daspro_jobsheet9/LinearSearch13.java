c6c8b1102718b9b5fb275e4c48dc5a21bdbc5cfc
Yeon <giojio825@gmail.com>
2023-10-25 10:27:25 +0700

    p2

--- daspro_jobsheet9/LinearSearch13.java
+++ daspro_jobsheet9/LinearSearch13.java
@@ -1,18 +1,30 @@
 package daspro_jobsheet9;
-
+import java.util.Scanner;
 public class LinearSearch13 {
     public static void main(String[] args) {
-        int[] arrayInt = {34,18,26,48,72,20,56,63};
-        int key = 20;
-        int hasil = 0;
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jum = input.nextInt();
+
+        int[] arrayInt = new int[jum];
+        int key;
+        int hasil=0;
+
+        for(int i = 0;i<arrayInt.length;i++){
+            System.out.print("Masukkan elemen array ke-" +i+" : ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = input.nextInt();
         for(int i=0;i<arrayInt.length; i++){
             if(arrayInt[i] == key){
                 hasil = i;
                 break;
             }
         }       
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);   
+        System.out.println("Key ada di posisi indeks ke-"+hasil);   
     }
 }
 
