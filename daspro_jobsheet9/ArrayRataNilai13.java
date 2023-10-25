2d1d5e2b9be7754eaa69c05238e7e8c14de56a70
Yeon <giojio825@gmail.com>
2023-10-25 10:04:12 +0700

    aa

--- daspro_jobsheet9/ArrayRataNilai13.java
+++ daspro_jobsheet9/ArrayRataNilai13.java
@@ -5,18 +5,27 @@ public class ArrayRataNilai13 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[]nilaiMhs = new int[10];
-        double total = 0, rata2;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jum = input.nextInt();
+
+        int[]nilaiMhs = new int[jum];
+        double lulus=0, tdkLulus=0;
+        int jumTdkLulus = 0, jumLulus = 0;
 
         for(int i=0;i<nilaiMhs.length;i++){
             System.out.print("Masukkan nilai mahasiswa ke- "+ (i+1)+": ");
             nilaiMhs[i] = input.nextInt();
+        if(nilaiMhs[i]>70){
+            lulus += nilaiMhs[i];
+            jumLulus++;                   
+        }else{
+            tdkLulus += nilaiMhs[i];
+            jumTdkLulus++;                      
+        }        
         }
-        for(int i =0;i<nilaiMhs.length; i++){
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
-
+        double rata2lulus = lulus/jumLulus;
+        System.out.println("Rata-rata nilai = "+rata2lulus);
+        double rata2tdkLulus = tdkLulus/jumTdkLulus;
+        System.out.println("Rata-rata nilai = "+rata2tdkLulus);  
     }
 }
