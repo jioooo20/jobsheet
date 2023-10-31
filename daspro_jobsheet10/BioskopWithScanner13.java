88b1a5560ff2f556b9354b4455aee1148cda4ba7
Yeon <giojio825@gmail.com>
2023-10-31 17:21:20 +0700

    10.2

--- daspro_jobsheet10/BioskopWithScanner13.java
+++ daspro_jobsheet10/BioskopWithScanner13.java
@@ -5,19 +5,38 @@ public class BioskopWithScanner13 {
         Scanner input = new Scanner(System.in);
 
         String nama, next;
-        int baris, kolom;
+        int baris, kolom, menu;
         String[][] penonton = new String[4][2];       
 
+        do{
+        System.out.println("\t Menu");
+        System.out.println("1. Input Data Penonton");
+        System.out.println("2. Tampilkan Daftar Penonton");
+        System.out.println("3. Exit");
+        System.out.print("Masukkan Nomer Menu: ");
+        menu = input.nextInt();
+        if (menu == 3 ) {
+            break;
+        }
+        switch (menu) {
+            case 1:
         while (true) {
+            System.out.println("===Input Data Penonton===");
             System.out.print("Masukkan nama: ");
-            nama = input.nextLine();
+            nama = input.next();
             System.out.print("Masukkan baris: ");
             baris = input.nextInt();
             System.out.print("Masukkan kolom: ");
             kolom = input.nextInt();
             input.nextLine();
             
+            if(baris<1 || baris> penonton.length || kolom < 1 || kolom > penonton[0].length){
+                System.out.println("Nomor Baris/Kolom Kursi Tidak Tersedia");
+                continue;
+            }
             penonton[baris-1][kolom-1] = nama;
+
+
             
             System.out.print("Input penonton lainnya? (y/n): ");
             next = input.nextLine();
@@ -25,7 +44,33 @@ public class BioskopWithScanner13 {
             if(next.equalsIgnoreCase("n")){
                 break;
             }
-        }
+        }                
+                break;//break menu1
 
+            case 2:
+            System.out.println("===Tampilkan Daftar Penonton===");
+            for (int i = 0; i < penonton.length; i++) {
+                for (int j = 0; j < penonton[i].length; j++) {
+                    System.out.println("Penonton pada baris " + (i+1) + ", kolom " + (j+1) + ": " + penonton[i][j]);
+                }
+            }
+                break;//break menu2
+        
+            default:
+            System.out.println("Masukkan Nomer yang ada pada menu!!!");
+                break; //break def
+        }
+        }while( menu != 3);
     }
 }
+
+
+            // for (int i = 0; i < penonton.length; i++) {
+            //     for (int j = 0; j < penonton[i].length; j++) {
+            //        if(penonton[i][j] == null){
+            //             penonton[baris-1][kolom-1] = nama;
+            //        }else{
+            //         System.out.println("Nomor Baris/Kolom Kursi Tidak Tersedia");
+            //        }
+            //     }
+            // }
