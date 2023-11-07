05a2df03aac70734cd65b21f53c51aae7398b5a7
Yeon <giojio825@gmail.com>
2023-11-07 12:19:20 +0700

    looparr1 amang

--- looparay1d1.java
+++ looparay1d1.java
@@ -4,10 +4,12 @@ public class looparay1d1 {
     public static void main(String[] args) {
         
         String[] alpha = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"};
+        int simpanIndex=0;
 
-        for(int i=0;i<15;i++){
+        for(int i=0;i<5;i++){
             for(int j=0;j<(i+1);j++){
-                System.out.print(alpha[j]);
+                System.out.print(alpha[simpanIndex]);
+                simpanIndex++;
             }
             System.out.println();
         }
