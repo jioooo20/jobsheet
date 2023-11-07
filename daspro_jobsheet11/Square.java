501a4b4fac08ae365513868369984659d024257c
Yeon <giojio825@gmail.com>
2023-11-07 23:44:05 +0700

    sqrdone

--- daspro_jobsheet11/Square.java
+++ daspro_jobsheet11/Square.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Square {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan nilai N+ ");
+        System.out.print("Masukkan nilai N = ");
         int N = input.nextInt();
-        for(int i=1;i<=N;i++){
-            System.out.println("*");
+        for(int iOuter=1; iOuter<=N; iOuter++){
+            for(int i=0;i<=N;i++){
+            System.out.print("*");
+            }
+        System.out.println();
         }
     }
 }
