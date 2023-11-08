54db7feceb2366bbc45b12b9ee388dbe4a189ece
Yeon <giojio825@gmail.com>
2023-11-08 09:46:41 +0700

    indiv1

---
+++ daspro_jobsheet7/se3_1.java
@@ -0,0 +1,23 @@
+package jobsheet.daspro_jobsheet7;
+import java.util.Scanner;
+public class se3_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int nilai;
+
+        System.out.print("Masukkan baris segitiga(Minimal 3 baris): ");
+        nilai = input.nextInt();
+
+        for (int i = 1; i <= nilai; i++) {
+            for (int j = 1; j <= nilai - i; j++) {
+            System.out.print(" ");
+            }
+            
+            for (int k = 1; k <= i; k++) {
+            System.out.print(k);
+            }
+            System.out.println();    
+        }        
+
+    }   
+}
