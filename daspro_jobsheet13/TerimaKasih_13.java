90642b942d7585c5cddf961689b2cc16a665bc8d
Yeon <giojio825@gmail.com>
2023-11-21 15:35:41 +0700

    percobaan 1

---
+++ daspro_jobsheet13/TerimaKasih_13.java
@@ -0,0 +1,13 @@
+package daspro_jobsheet13;
+
+public class TerimaKasih_13 {
+
+    public static void UcapanTerimakasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n" +
+                "You inspired in me a love for learning and made me feel like I could ask you everything.");
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimakasih();
+    }
+}
