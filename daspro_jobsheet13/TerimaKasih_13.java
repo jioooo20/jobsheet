5f4d7d17067bfd554be22b957737ca82984e62a7
Yeon <giojio825@gmail.com>
2023-11-21 17:03:14 +0700

    pertanyaan2

--- daspro_jobsheet13/TerimaKasih_13.java
+++ daspro_jobsheet13/TerimaKasih_13.java
@@ -7,10 +7,17 @@ public class TerimaKasih_13 {
                 + "You inspired in me a love for learning and made me feel like I could ask you everything.");
     }
 
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+
     public static void main(String[] args) {
         UcapanTerimakasih();
+        String ucapan = "Terima kasih pak.. bu.. semoga sehat selalu";
+        UcapanTambahan(ucapan);
     }
 }
+
 // public class TerimaKasih_13 {
 // public static void main(String[] args) {
 // System.out.println("Thank you for being the best teacher in the world.\n" +
